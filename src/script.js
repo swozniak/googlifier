22eccc6a4163e2bcc71f0880e4ceb8f0bce9c2ba
Martin <m.tschirsich@gmx.de>
2015-02-20 04:29:19 +0100

    - Split observer into attrib and childList observer

--- src/script.js
+++ src/script.js
@@ -21,33 +21,32 @@ for (var i = 0; i < images.length; ++i) {
 	googlify(images[i]);
 }
 
-/** Googlify all subsequently added or modified images */
-var observer = new MutationObserver(function(mutations) {
+/** Googlify subsequently added images */
+var childListObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-		if (mutation.type === 'attributes') {
-			
-			// Googlify images whose src changed
-			if (mutation.target.tagName === 'IMG' && mutation.attributeName === 'src') {
-				googlify(mutation.target);
-			}
-		} else if (mutation.type === 'childList') {
-			
-			// Googlify images that have been added
-			for (var j = 0; j < mutation.addedNodes.length; ++j) {
-				var node = mutation.addedNodes[j];
-				if (node.nodeType === 1) {
-					var images = node.getElementsByTagName('IMG');
-					for (var i = 0; i < images.length; ++i) {
-						googlify(images[i]);
-					}
+    	
+		// Googlify images that have been added
+		for (var j = 0; j < mutation.addedNodes.length; ++j) {
+			var node = mutation.addedNodes[j];
+			if (node.nodeType === 1) {
+				var images = node.getElementsByTagName('IMG');
+				for (var i = 0; i < images.length; ++i) {
+					googlify(images[i]);
 				}
 			}
 		}
 	});
 });
 
-observer.observe(document.body, {
-	childList: true,
-	attributes: true,
-	subtree: true
+childListObserver.observe(document.body, {childList: true, subtree: true});
+
+/** Googlify subsequently modified images with new src */
+var attributeObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+		if (mutation.target.tagName === 'IMG' && mutation.attributeName === 'src') {
+			googlify(mutation.target);
+		}
+	});
 });
+
+attributeObserver.observe(document.body, {attributes: true,	subtree: true});
