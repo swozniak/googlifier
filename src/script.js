a13411fb188f7a44220508435a533476f3d617a8
Martin <m.tschirsich@gmx.de>
2015-02-20 04:21:06 +0100

    - Detection of eyes only within region of interest in detected faces - Fixed event handling, googlify added and modified images - Limit maximum detector resolution to 200 px - Removed jquery dependency - ...

--- src/script.js
+++ src/script.js
@@ -1,36 +1,53 @@
-function googlifyImage(img, force) {
+/** Googlify image in background thread */
+function googlify(img) {
+	// Skip if already googlified once
+	if (!img.src || img.googlified) return;
+	img.googlified = true;
+
+	// Perform googlification in background
 	chrome.runtime.sendMessage({
-		'type': 'processImage',
+		'type': 'googlify',
 		'src': img.src
-	}, function (response) {
+	}, function(response) {
 		if (response.googlified) {
 			img.src = response.googlified;
 		}
 	});
 }
 
-$(function () {
-	var observerOptions = {
-			childList: true,
-			attributes: false,
-			characterData: false,
-			subtree: true
-		},
-		observer = new MutationObserver(function (mutations) {
-			mutations.forEach(function (mutation) {
-				if (mutation.addedNodes) {
-					$(mutation.addedNodes).each(function () {
-						$('img', this).each(function () {
-							googlifyImage(this, false);
-						});
-					});
+/** Googlify all available images on initialization */
+var images = document.getElementsByTagName('IMG');
+for (var i = 0; i < images.length; ++i) {
+	googlify(images[i]);
+}
+
+/** Googlify all subsequently added or modified images */
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+		if (mutation.type === 'attributes') {
+			
+			// Googlify images whose src changed
+			if (mutation.target.tagName === 'IMG' && mutation.attributeName === 'src') {
+				googlify(mutation.target);
+			}
+		} else if (mutation.type === 'childList') {
+			
+			// Googlify images that have been added
+			for (var j = 0; j < mutation.addedNodes.length; ++j) {
+				var node = mutation.addedNodes[j];
+				if (node.nodeType === 1) {
+					var images = node.getElementsByTagName('IMG');
+					for (var i = 0; i < images.length; ++i) {
+						googlify(images[i]);
+					}
 				}
-			});
-		});
-
-	observer.observe(document.querySelector('body'), observerOptions);
-
-	$('img').each(function () {
-		googlifyImage(this, false);
+			}
+		}
 	});
+});
+
+observer.observe(document.body, {
+	childList: true,
+	attributes: true,
+	subtree: true
 });
